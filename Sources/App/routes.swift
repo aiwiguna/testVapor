905139bdf9cebdccf68906c431cad54fc2268361
Antoni Wiguna <antoni.wiguna@gmail.com>
2018-09-13 11:10:34 +0700

    update routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -9,7 +9,7 @@ public func routes(_ router: Router) throws {
 
     // Example of configuring a controller
     let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
+    router.get("todo", use: todoController.index)
+    router.post("todo", use: todoController.create)
+    router.delete("todo", Todo.parameter, use: todoController.delete)
 }
